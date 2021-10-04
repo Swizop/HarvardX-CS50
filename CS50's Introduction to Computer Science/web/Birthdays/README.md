131381865491fe1f1da6a5fa4e6226a0cd0188fd
Neagu Matei <matei.neagu19@yahoo.com>
2021-10-04 19:17:15 +0300

    first commit

---
+++ CS50's Introduction to Computer Science/web/Birthdays/README.md
@@ -0,0 +1,1 @@
+Web application for keeping track of friends' birthdays. Made with Flask
---
+++ CS50's Introduction to Computer Science/web/Birthdays/application.py
@@ -0,0 +1,42 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+#cmd python -m flask run in order to run web app
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+def is_valid_date(month, day):
+    day_count_for_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    # if year%4==0 and (year%100 != 0 or year%400==0):
+    #     day_count_for_month[2] = 29
+    return (1 <= month <= 12 and 1 <= day <= day_count_for_month[month])
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        month = request.form.get("month")
+        day = request.form.get("day")
+        name = request.form.get("name")
+
+        if month.isnumeric() and day.isnumeric() and name.isalpha() and is_valid_date(int(month), int(day)):
+            db.execute("INSERT INTO birthdays (name, month, day) VALUES(?, ?, ?)", name, month, day)
+
+        return redirect("/")
+
+    else:
+
+        birthdays = db.execute("SELECT * FROM birthdays")
+
+        return render_template("index.html", birthdays = birthdays)
+
+
